ce3d013367cb63b45f8e0f6cd7de948d30d2907c
ligiatirado <ligiatirado@users.noreply.github.com>
2022-12-21 22:06:01 +0000

    inicial

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
+import Navbar from './components/navbar/navbar';
+import Banner from './components/banner/banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
