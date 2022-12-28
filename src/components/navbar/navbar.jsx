ca461f74c12cdc33568f4a5337198bb94eaf08e7
ligiatirado <ligiatirado@users.noreply.github.com>
2022-12-28 02:23:01 +0000

    definicao de logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,15 +2,17 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./navbar.css";
+import logo from "../../assets/images/logo.png"
 
 function NavegationBar() {
   return (
     <Navbar className="shadow-sm container-navbar">
-      <Container fluid="false">
+      <img className="logo-nav" src={logo} alt="" />
+      <Container>
         <Nav className="ms-auto">
           <Nav.Link href="#experts">Experts</Nav.Link>
           <Nav.Link href="#about-us">About Us</Nav.Link>
-          <h3> | </h3>
+          <h2> | </h2>
           <Nav.Link href="#sign-in">Sign In</Nav.Link>
           <Nav.Link href="#sign-up">Sign Up</Nav.Link>
         </Nav>
